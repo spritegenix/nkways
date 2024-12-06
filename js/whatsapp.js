945709e0a9e7e24899d847a0997ad814f03430cd
Ashik <ashikshettyc@gmail.com>
2024-12-06 13:36:10 +0530

    whatsapp icon added

---
+++ js/whatsapp.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function () {
+  const floating_btn = document.querySelector('#floating_btn');
+  floating_btn.innerHTML = `<a target="_blank" href="https://wa.me/+27635823087" aria-label="Whatsapp us">
+        <div class="contact_icon">
+          <img src="/img/whatsapp.png" alt="">
+        </div>
+      </a>`;
+});
